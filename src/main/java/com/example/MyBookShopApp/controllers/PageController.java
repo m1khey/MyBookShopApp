039c89c6d7b8f03169d0a1846aecefdf4319333c
user <ermo1ov@yandex.ru>
2021-10-01 05:37:31 +0300

    Замечания преподавателя после выполнения задания №3

---
+++ src/main/java/com/example/MyBookShopApp/controllers/PageController.java
@@ -0,0 +1,17 @@
+package com.example.MyBookShopApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PageController {
+
+    @GetMapping("/")
+    public String page(Model model){
+
+        return "redirect:/bookshop/main";
+    }
+}
