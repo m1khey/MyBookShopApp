f1b8cae2b6b75a8e54070fb54e7e71068e833ef1
user <ermo1ov@yandex.ru>
2021-10-24 23:47:49 +0300

    mod 4.2

---
+++ src/main/java/com/example/MyBookShopApp/config/LocalChangeConfig.java
@@ -0,0 +1,32 @@
+package com.example.MyBookShopApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+public class LocalChangeConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localResolver(){
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.ENGLISH);
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
--- src/main/java/com/example/MyBookShopApp/controllers/PageController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/PageController.java
@@ -1,5 +1,6 @@
 package com.example.MyBookShopApp.controllers;
 
+import org.springframework.beans.factory.support.GenericTypeAwareAutowireCandidateResolver;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
